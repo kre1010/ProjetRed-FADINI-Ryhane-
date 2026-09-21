4eb5f69c92e7c06981f05c9993e6dd874a8a546c
Fadini Ryhane <ryhane.fadini@ynov.com>
2026-09-21 11:50:17 +0200

    wasted fix

--- src/character/wasted.go
+++ src/character/wasted.go
@@ -3,36 +3,36 @@ package character
 import "fmt"
 
 type character struct {
-      Name string
-      Pv int
-      Alive bool
+	Name  string
+	Pv    int
+	Alive bool
 }
 
 func Damage(c *character, amount int) {
 
-if !c.Alive {
-    fmt.Println(c.Name, "is already dead.")
-              return
-        }
-             c.Pv -= amount
-             if c.Pv <= 0 {
-                c.Pv = 0
-                c.Alive = false
+	if !c.Alive {
+		fmt.Println(c.Name, "is already dead.")
+		return
+	}
+	c.Pv -= amount
+	if c.Pv <= 0 {
+		c.Pv = 0
+		c.Alive = false
 
-            fmt.Println(c.Name, "you died !")
-            } else {
-             fmt.Printf("%s has %d HP remaining.\n", c.Name, c.Pv)
-        }
-    }
+		fmt.Println(c.Name, "you died !")
+	} else {
+		fmt.Printf("%s has %d HP remaining.\n", c.Name, c.Pv)
+	}
+}
 
-    func main() {
-	    player := character{
-            Name: "c.class",
-               Pv: 100,
-               Alive: true,
-		}
-      		Damage(&player, 30)
-            Damage(&player, 50)
-            Damage(&player, 25)
-            Damage(&player, 10)
+func main() {
+	player := character{
+		Name:  "c.class",
+		Pv:    100,
+		Alive: true,
+	}
+	Damage(&player, 30)
+	Damage(&player, 50)
+	Damage(&player, 25)
+	Damage(&player, 10)
 }
