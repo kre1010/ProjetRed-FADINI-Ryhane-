ec7c80e95433876c97b9d482beb9aa44a77461fc
Killian MORETTE <killian.morette@ynov.com>
2026-09-22 14:22:56 +0200

    feat  accessinventory

--- src/inventory/inventory.go
+++ src/inventory/inventory.go
@@ -2,21 +2,10 @@ package inventory
 
 import "fmt"
 
-var inventory []string
+var inventory map[string]int
 
 func AccessInventory() {
 	fmt.Println("Inventaire :")
-
-	for i := 0; i < len(inventory); i++ {
-		fmt.Println(i+1, "-", inventory[i])
-	}
+	fmt.Println("-", inventory)
 }
 
-func buyItem(item string) {
-	if len(inventory) >= 10 {
-		fmt.Println("L'inventaire est plein !")
-	} else {
-		inventory = append(inventory, item)
-		fmt.Println(item, "a été ajouté à l'inventaire.")
-	}
-}
